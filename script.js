71818a3573ba67304cc4f987e449da2b33ee5632
Syed Irfan Amjad Abidi <irfanabidi7@gmail.com>
2021-09-04 00:44:50 +0530

    Files Moved

---
+++ script.js
@@ -0,0 +1,22 @@
+function readMore(desc) {
+    if (desc == 'proj-1') {
+        var dots = document.getElementById("dots");
+        var moreText = document.getElementById("more");
+        var btnText = document.getElementById("btn-read-" + desc);
+        if (dots.style.display === "none") {
+            dots.style.display = "inline";
+            btnText.innerHTML = "Read more";
+            moreText.style.display = "none";
+        } else {
+            dots.style.display = "none";
+            btnText.innerHTML = "Read less";
+            moreText.style.display = "inline";
+        }
+    }
+}
+$(".navbar-collapse a").click(function() {
+    $(".navbar-collapse").collapse("hide");
+});
+$(".navbar-toggler-icon").click(function() {
+    $(".navbar-collapse").collapse("hide");
+});
